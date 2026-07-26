fde26230769f0e86aa17c1c2082f1b916536b5a3
Kelly <kelly@kellywilson.us>
2026-07-26 13:22:27 -0700

    some more changes

--- src/components/InsightsDashboard.tsx
+++ src/components/InsightsDashboard.tsx
@@ -188,7 +188,7 @@ export default function InsightsDashboard({childLevelData, placementLevelData, p
               component="div"
               sx={{ marginBottom: 3 }}
             >
-              Providers with placements ----
+              Providers with placements
             </Typography>
             <Typography variant="h1" sx={{ marginBottom: 2, color: 'blue' }}>
               {calculateActivePlacements(placementLevelData)}
