70e4f45c44c235d59fc91d233b54bd5681ed17b2
Kelly <kelly@kellywilson.us>
2026-07-26 13:32:00 -0700

    updating rsbuild config! maybe I have it this time!!!

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -8,4 +8,9 @@ export default defineConfig({
       reactCompiler: true,
     }),
   ],
+  output: {
+    // Please replace <REPO_NAME> with the repository name.
+    // For example, "/my-project/"
+    assetPrefix: '/foster-insights-interview-app/',
+  },
 });
